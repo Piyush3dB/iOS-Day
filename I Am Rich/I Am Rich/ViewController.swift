fa48a70edcbd006874b40a80cf440846491ce965
Piyush Singh <piyushsingh@Piyushs-MacBook-Pro.local>
2016-03-05 11:07:16 +0000

    Test app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Piyush Singh on 05/03/2016.
+//  Copyright © 2016 Piyush Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
